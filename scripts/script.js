bbf7330c49cd0330f35e461fe396490a781317f6
Roger Zhuang <zhuangziyin@gmail.com>
2022-11-27 21:01:57 -0800

    add array and implement save functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 buttonA = document.querySelector(".button-a");
 buttonB = document.querySelector(".button-b");
+buttonC = document.querySelector(".button-c");
 buttonD = document.querySelector(".button-d");
 textArea = document.querySelector("textarea");
 classList1 = [".item-a", ".item-b", "textarea", ".button-a", ".button-b", ".button-c", ".button-d"];
 classList2 = ["textarea", ".button-c", ".button-d"];
-
+notesArray = [{title:"note one", body:"this is my first note"}, {title:"note two", body:"this is my second note"}];
 
 buttonB.addEventListener("click", () => {
     for (let item of classList1) {
@@ -34,3 +35,16 @@ buttonA.addEventListener("click", () => {
         document.querySelector(`${item}`).classList.remove("hide");
     }
 })
+
+buttonC.addEventListener("click", () => {
+    if (textArea.value !== "") {
+        alert("Please input a title!");
+        input = prompt("What do you want to name your note?");
+        listItem = document.createElement("li");
+        listItem.innerText = input;
+        document.querySelector(".item-b").appendChild(listItem);
+        note = {title:input, body:textArea.value};
+        notesArray.push(note);
+    }
+})
+
