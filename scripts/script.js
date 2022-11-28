3ffb504c9a1c39134e8d9270741f614d6dfab867
Roger Zhuang <zhuangziyin@gmail.com>
2022-11-27 20:22:41 -0800

    implement cancel and add

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,36 @@
+buttonA = document.querySelector(".button-a");
 buttonB = document.querySelector(".button-b");
-classList = [".item-a", ".item-b", "textarea", ".button-a", ".button-b", ".button-c", ".button-d"];
+buttonD = document.querySelector(".button-d");
+textArea = document.querySelector("textarea");
+classList1 = [".item-a", ".item-b", "textarea", ".button-a", ".button-b", ".button-c", ".button-d"];
+classList2 = ["textarea", ".button-c", ".button-d"];
 
-buttonB.addEventListener("click", ()=>{
-    for (let item of classList) {
+
+buttonB.addEventListener("click", () => {
+    for (let item of classList1) {
         document.querySelector(`${item}`).classList.toggle("darkBGColor");
     }
-    
+
     if (buttonB.textContent === "Dark Theme") {
         buttonB.textContent = "Light Theme";
     }
     else {
         buttonB.textContent = "Dark Theme";
+    }
+})
+
+buttonD.addEventListener("click", () => {
+    for (let item of classList2) {
+        document.querySelector(`${item}`).classList.add("hide");
+    }
+})
+
+buttonA.addEventListener("click", () => {
+    if (!textArea.classList.contains("hide")) {
+        textArea.value = "";
+    }
+
+    for (let item of classList2) {
+        document.querySelector(`${item}`).classList.remove("hide");
     }
 })
